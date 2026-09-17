b431b0219f48c8298eedb1d0f99afa288636ef10
Spring <dlqhadl58@naver.com>
2026-09-17 17:41:27 +0100

    repository __init__.py

---
+++ app/domain/repository interface/__init__.py
@@ -0,0 +1,9 @@
+from app.domain.repository_interfaces.material_repository import MaterialRepositoryInterface
+from app.domain.repository_interfaces.product_repository import ProductRepositoryInterface
+from app.domain.repository_interfaces.stock_log_repository import StockLogRepositoryInterface
+
+__all__ = [
+    "MaterialRepositoryInterface",
+    "ProductRepositoryInterface",
+    "StockLogRepositoryInterface",
+]

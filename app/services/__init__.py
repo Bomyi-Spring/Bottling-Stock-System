2498f367026cc23cc57948274d7378cb8771ef1e
Spring <dlqhadl58@naver.com>
2026-09-18 17:24:32 +0100

    service__innit__

---
+++ app/services/__init__.py
@@ -0,0 +1,9 @@
+from app.services.material_service import MaterialService
+from app.services.product_service import ProductService
+from app.services.stock_log_service import StockLogService
+
+__all__ = [
+    "MaterialService",
+    "ProductService",
+    "StockLogService",
+]
